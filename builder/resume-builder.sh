8bff995ef87148775b5098ce715524ab3994043e
Ryan Gates <ryanvgates@gmail.com>
2024-06-02 16:01:32 -0400

    use printf rather than echo for better output default working directory to the current directory

--- builder/resume-builder.sh
+++ builder/resume-builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eo pipefail
+set -eou pipefail
 
 while [[ $# -gt 0 ]]; do
   case $1 in
@@ -25,11 +25,11 @@ while [[ $# -gt 0 ]]; do
       shift # past value
       ;;
     -wd|--working-directory)
-      WORKING_DIRECTORY="$2"
+      WORKING_DIRECTORY="${2:-$(pwd)}"
       shift # past argument
       shift # past value
       ;;
-    -*|--*)
+    -*)
       echo "Unknown option $1"
       exit 1
       ;;
@@ -41,21 +41,20 @@ while [[ $# -gt 0 ]]; do
 done
 
 printf '=%.0s' {1..100}
-echo ""
-echo "TEMPLATE DIRECTORY    = ${TEMPLATE_DIRECTORY}"
-echo "COMPANY               = ${COMPANY}"
-echo "ROLE                  = ${ROLE}"
-echo "ID                    = ${ID}"
-echo "WORKING DIRECTORY     = ${WORKING_DIRECTORY}"
+printf "TEMPLATE DIRECTORY    = %s\n" "$TEMPLATE_DIRECTORY"
+printf "COMPANY               = %s\n" "$COMPANY"
+printf "ROLE                  = %s\n" "$ROLE"
+printf "ID                    = %s\n" "$ID"
+printf "WORKING DIRECTORY     = %s\n" "$WORKING_DIRECTORY"
 printf '=%.0s' {1..100}
-echo ""
+printf "\n"
 
 APPLICATION_PATH="${WORKING_DIRECTORY}/${COMPANY}/${ROLE}/${ID}"
 
-echo "Creating directory ${APPLICATION_PATH}"
-mkdir --parents "${APPLICATION_PATH}"
+printf "Creating directory %s\n" "$APPLICATION_PATH"
+mkdir --parents "$APPLICATION_PATH"
 
-echo "Copying *.tex templates from ${TEMPLATE_DIRECTORY} to ${APPLICATION_PATH}"
+printf "Copying *.tex templates from %s to %s \n" "$TEMPLATE_DIRECTORY" "$APPLICATION_PATH"
 cp -r "${TEMPLATE_DIRECTORY}/"*.tex "${TEMPLATE_DIRECTORY}/enumitem.sty" "${APPLICATION_PATH}"
 
 exit 0;
